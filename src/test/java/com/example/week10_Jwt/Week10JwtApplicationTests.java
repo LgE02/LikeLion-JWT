9e176a7d0a34c686d3bc49104c111a6bd94a0cdb
LgE02 <leegaeun023@gmail.com>
2025-07-22 18:12:11 +0900

    기본 세팅

---
+++ src/test/java/com/example/week10_Jwt/Week10JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week10_Jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
