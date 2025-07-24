a25b93c3b91f093a507b46ce912943e419201dfe
LgE02 <leegaeun023@gmail.com>
2025-07-24 23:54:08 +0900

    :sparkles: 로그인 검증 로직

---
+++ src/main/java/com/example/week10_Jwt/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.example.week10_Jwt.service;
+
+import com.example.week10_Jwt.dto.CustomUserDetails;
+import com.example.week10_Jwt.entity.UserEntity;
+import com.example.week10_Jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if(username != null){
+
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
